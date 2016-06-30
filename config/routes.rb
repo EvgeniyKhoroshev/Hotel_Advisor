b234501be2ccb66b142bda3584d32f9c7ac6684e
Alexey Batrachenko <zzzaqxswcdevfrbgt98@gmail.com>
2016-06-30 14:06:02 +0300

    add index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'hotels#index'
+  resource :hotels
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
